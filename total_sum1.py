ce23144729adc11e7679895023599b063ed902f2
vicia81 <pvk.sakha@yandex.ru>
2021-04-23 15:09:34 +0900

    first commit

---
+++ total_sum1.py
@@ -0,0 +1,19 @@
+total_sum = 0
+for i in range(0, 101):
+    total_sum += i
+print(total_sum)
+
+
+total_sum_chetnih = 0
+for t in range(0, 101):
+    if t % 2 == 0:
+        total_sum_chetnih += t
+print(total_sum_chetnih)
+
+
+total_sum_cratnui_semi = 0
+for b in range(0, 101):
+    if b % 7 == 0:
+        total_sum_cratnui_semi += b
+print(total_sum_cratnui_semi)
+
